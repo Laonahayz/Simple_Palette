e8e4ca1bc501ef919cc7b0b2ae58216d6195627e
Laonahayz <y67989@gmail.com>
2025-05-23 17:17:36 +0800

    upload Simple_Palette

---
+++ Simple_Palette/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Simple_Palette
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.MainColorPanel = new System.Windows.Forms.Panel();
+            this.Instructions = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 453);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(876, 107);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // MainColorPanel
+            // 
+            this.MainColorPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MainColorPanel.Location = new System.Drawing.Point(12, 40);
+            this.MainColorPanel.Name = "MainColorPanel";
+            this.MainColorPanel.Size = new System.Drawing.Size(876, 390);
+            this.MainColorPanel.TabIndex = 1;
+            this.MainColorPanel.Click += new System.EventHandler(this.MainColorPanel_Click);
+            this.MainColorPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainColorPanel_MouseMove);
+            // 
+            // Instructions
+            // 
+            this.Instructions.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Instructions.Location = new System.Drawing.Point(12, 9);
+            this.Instructions.Name = "Instructions";
+            this.Instructions.Size = new System.Drawing.Size(876, 22);
+            this.Instructions.TabIndex = 2;
+            this.Instructions.Text = "Instructions Text";
+            this.Instructions.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(900, 572);
+            this.Controls.Add(this.Instructions);
+            this.Controls.Add(this.MainColorPanel);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "簡易調色盤";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel MainColorPanel;
+        private System.Windows.Forms.Label Instructions;
+    }
+}
+
---
+++ Simple_Palette/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_Palette
+{
+    public partial class Form1 : Form
+    {
+        int x = 0;
+        int y = 0;
+        int blue = 0;
+        public Form1()
+        {
+            MinimumSize = new Size(918, 619);
+            MaximumSize = new Size(918, 619);
+            InitializeComponent();
+            MouseWheel += MainColorPanel_MouseWheel; //控制滾輪變化藍色
+            Instructions.Text = "滑鼠移動：改變顏色　｜　滾輪：調整藍色值　｜　點擊：加入色塊";
+        }
+
+        private void MainColorPanel_Click(object sender, EventArgs e)
+        {
+            Color bgColor = MainColorPanel.BackColor;
+            int maxColors = 6;
+            int r = bgColor.R;
+            int g = bgColor.G;
+            int b = bgColor.B;
+            
+            string rgbText = "RGB(" + r + "," + g + "," + b + ")";
+            string hexText = "#" + r.ToString("X2") + g.ToString("X2") + b.ToString("X2");
+
+            // 色塊Panel
+            Panel newColorPanel = new Panel();
+            newColorPanel.Size = new Size(50, 50);
+            newColorPanel.BackColor = bgColor;
+            newColorPanel.Location = new Point(25, 0);
+            // 顏色label
+            Label colorLabel = new Label();
+            colorLabel.Text = rgbText + "\n" + hexText;
+            colorLabel.AutoSize = false;
+            colorLabel.Size = new Size(100, 80); // 保持容器大小
+            colorLabel.Font = new Font("Arial", 8);
+            colorLabel.Location = new Point(0, 55);
+            colorLabel.TextAlign = ContentAlignment.TopCenter;
+
+            // container包色塊、文字
+            Panel container = new Panel();
+            container.Size = new Size(100, 80);
+            container.Margin = new Padding(4);
+            container.Controls.Add(newColorPanel);
+            container.Controls.Add(colorLabel);
+            
+            if (flowLayoutPanel1.Controls.Count >= maxColors)
+            {
+                flowLayoutPanel1.Controls.RemoveAt(0); // 移除最舊的那一個
+            }
+            flowLayoutPanel1.Controls.Add(container);
+        }
+
+        private void MainColorPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            x = e.Location.X;
+            y = e.Location.Y;
+            int red = (int)(255 * (e.X / (float)MainColorPanel.Width));
+            int green = (int)(255 * (e.Y / (float)MainColorPanel.Height));
+
+            MainColorPanel.BackColor = Color.FromArgb(red, green, blue);
+        }
+
+        private void MainColorPanel_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (e.Delta > 0)    // 往上滾輪，增加藍色值
+            {
+                blue += 5;
+            }
+            else
+            {
+                blue -= 5;      // 往下滾輪，減少藍色值
+            }
+            if (blue < 0)
+            {
+                blue = 0;
+            }
+            else if (blue > 255)
+            {
+                blue = 255;
+            }
+
+        }
+
+    }
+}
